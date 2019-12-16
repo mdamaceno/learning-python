233e58dd9206ce3c062fdb2270a15946940fc793
Marco Damaceno <maadamaceno@gmail.com>
2019-12-15 21:55:42 -0300

    refactoring

--- myapp/forms/signup.py
+++ myapp/forms/signup.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 
 class SignupForm(FlaskForm):
-  name = StringField('name')
-  email = StringField('email')
-  password = PasswordField('password')
+  name = StringField('name', [DataRequired()])
+  email = StringField('email', [DataRequired(), Email()])
+  password = PasswordField('password', [DataRequired(), Length(min=6)])
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,8 +1,50 @@
-from app import app
-from myapp.controllers import users_controller
+from flask import render_template, request, redirect, url_for, flash
+import bcrypt
+from app import app, db
+from myapp.models.user import User
+from myapp.forms.signup import SignupForm
 
-app.add_url_rule('/', 'home', view_func=users_controller.index)
-app.add_url_rule('/signin', 'signin', view_func=users_controller.signin)
-app.add_url_rule('/signup', 'signup', view_func=users_controller.signup, methods=['GET', 'POST'])
-app.add_url_rule('/dashboard', 'dashboard', view_func=users_controller.dashboard)
-app.add_url_rule('/user', 'user', view_func=users_controller.user)
+@app.route('/')
+def index():
+  return render_template('home.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+  return render_template('signin.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  inputs = SignupForm(request.form)
+  # POST: Create user
+  if request.method == 'POST':
+    # Validation
+    if inputs.validate():
+      # Get form fields
+      name = request.form.get('name')
+      email = request.form.get('email')
+      password = request.form.get('password')
+      # Check if user already exists
+      existing_user = User.query.filter_by(email=email).first()
+      if existing_user is None:
+        # Save the user
+        user = User(
+          name=name,
+          email=email,
+          password=bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+      flash('A user already exists with that email address.')
+      return redirect(url_for('signup'))
+  # GET: Serve sign up page
+  form = SignupForm()
+  return render_template('signup.html', form=SignupForm())
+
+@app.route('/dashboard')
+def dashboard():
+  return render_template('dashboard.html')
+
+@app.route('/user')
+def user():
+  return render_template('user.html')
